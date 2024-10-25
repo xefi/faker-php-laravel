0735548b8a54eb31cdf24638fb3ee34daa384abc
Gautier DELEGLISE <gautier.deleglise@gmail.com>
2024-10-25 13:35:29 +0200

    :sparkles: bootstrap faker packages file

--- src/FakerLaravelServiceProvider.php
+++ src/FakerLaravelServiceProvider.php
@@ -3,9 +3,20 @@ namespace Xefi\Faker\Laravel;
 
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\Facades\Config;
+use Xefi\Faker\Container\Container;
 
 class FakerLaravelServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Container::packageManifestPath('./bootstrap/cache/faker-packages.php');
+    }
+
     /**
      * Register the service provider.
      *
